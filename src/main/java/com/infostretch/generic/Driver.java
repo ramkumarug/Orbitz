761b8f39a8b390bb7a914008b344e8eb188663ed
ramkumarug@gmail.com <ramkumarug@gmail.com>
2018-02-02 18:16:15 +0530

    1

--- src/main/java/com/infostretch/generic/Driver.java
+++ src/main/java/com/infostretch/generic/Driver.java
@@ -1,11 +1,11 @@
 package com.infostretch.generic;
 
 import java.util.concurrent.TimeUnit;
-import com.infostretch.generic.Const;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
 
 public class Driver implements Const {
@@ -15,7 +15,7 @@ public class Driver implements Const {
 		System.setProperty(CHROME_KEY,CHROME_VALUE);
 	}
 
-	@BeforeMethod
+	@BeforeClass
 	public void openApplication()
 	{
 	    driver =new ChromeDriver();
@@ -24,7 +24,7 @@ public class Driver implements Const {
 		driver.get(URL);
 	}
 	
-	@AfterMethod
+	@AfterClass
 	public void closeApplication()
 	{
 		driver.close();
